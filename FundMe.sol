536c047a476958d5b49c0a1a9b9404b52549ceda
albahaca0000 <144887756+albahaca0000@users.noreply.github.com>
2023-12-31 17:32:48 +0430

    Update FundMe.sol

--- FundMe.sol
+++ FundMe.sol
@@ -8,13 +8,39 @@ contract FundMe {
 
     uint256 public minimumUsd = 5e18;
 
+    address public owner;
+
     address[] public funders;
     mapping(address funder => uint256 AmountFunded) public addressToAmountFunded;
+
+    constructor() {
+        owner = msg.sender;
+    }
 
     function fund() public payable {
        require(msg.value.getConversionRate() >= minimumUsd, "didn't send enough ETH");
        funders.push(msg.sender);
-       addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+       addressToAmountFunded[msg.sender] += msg.value;
     }
+
+    modifier onlyOwner() {
+       require(msg.sender == owner, "sender must be owner");
+       _;
+    }
+
+    function withdraw() public onlyOwner {
+        
+        for (uint256 fundersIndex = 0;fundersIndex >= funders.length; fundersIndex++) {
+           address funder = funders[fundersIndex];
+           addressToAmountFunded[funder] = 0;
+        }
+
+        funders = new address[](0);
+
+        (bool success, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(success, "Call failed");
+    }
+
+
 
 }
